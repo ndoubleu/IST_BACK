8dd253977b6e6b770bd537cf09b96d8ac9f6a7a7
ndoubleu <65906953+ndoubleu@users.noreply.github.com>
2022-03-03 19:29:39 +0300

    Update urls.py

--- back/urls.py
+++ back/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # path('api/auth/', include('rest_framework.urls')),
     # path('api/rest-auth/', include('rest_auth.urls')),
     # path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
