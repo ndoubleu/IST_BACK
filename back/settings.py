7b6ec61669b68d917fe49476ef78240e1de382b7
ndoubleu <65906953+ndoubleu@users.noreply.github.com>
2022-03-03 18:16:58 +0300

    settings

--- back/settings.py
+++ back/settings.py
@@ -25,9 +25,9 @@ SITE_ID = 1 # new
 SECRET_KEY = 'django-insecure-!p@ryu#%c)b3)7v#nt60nnr0(8mgmtf1aht7)yks1ipf&i2^lu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'ist-back.herokuapp.com']
 
 
 # Application definition
@@ -51,22 +51,21 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'rest_framework_simplejwt',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'back.urls'
-CORS_ALLOW_ALL_ORIGINS = True
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -157,35 +156,38 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
-    'UPDATE_LAST_LOGIN': False,
+JWT_AUTH = {
+'JWT_ENCODE_HANDLER':
+'rest_framework_jwt.utils.jwt_encode_handler',
 
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': SECRET_KEY,
-    'VERIFYING_KEY': None,
-    'AUDIENCE': None,
-    'ISSUER': None,
-    'JWK_URL': None,
-    'LEEWAY': 0,
+'JWT_DECODE_HANDLER':
+'rest_framework_jwt.utils.jwt_decode_handler',
 
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
-    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+'JWT_PAYLOAD_HANDLER':
+'rest_framework_jwt.utils.jwt_payload_handler',
 
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
-    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
 
-    'JTI_CLAIM': 'jti',
+'JWT_RESPONSE_PAYLOAD_HANDLER':
+'rest_framework_jwt.utils.jwt_response_payload_handler',
 
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(days=1),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+'JWT_SECRET_KEY': settings.SECRET_KEY,
+'JWT_GET_USER_SECRET_KEY': None,
+'JWT_PUBLIC_KEY': None,
+'JWT_PRIVATE_KEY': None,
+'JWT_ALGORITHM': 'HS256',
+'JWT_VERIFY': True,
+'JWT_VERIFY_EXPIRATION': True,
+'JWT_LEEWAY': 0,
+'JWT_EXPIRATION_DELTA': timedelta(days=10),
+'JWT_AUDIENCE': None,
+'JWT_ISSUER': None,
+
+'JWT_ALLOW_REFRESH': False,
+'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=30),
+
+'JWT_AUTH_HEADER_PREFIX': 'JWT',
+'JWT_AUTH_COOKIE': None,
+
 }
-
